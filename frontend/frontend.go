b471e3c4c8a4f4da3c3b2ec2980c2a6420160384
Diego Carretero-Frades <carter@diektronics.com>
2014-04-11 23:04:05 -0700

    some blobstoring

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -1,11 +1,14 @@
 package frontend
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"net/url"
 	"time"
 
 	"appengine"
+	"appengine/blobstore"
 	"appengine/datastore"
 	"appengine/user"
 )
@@ -19,6 +22,15 @@ type Greeting struct {
 func init() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/sign", sign)
+	http.HandleFunc("/root", handleRoot)
+	http.HandleFunc("/upload", handleUpload)
+}
+
+func serveError(c appengine.Context, w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, "Internal Server Error")
+	c.Errorf("%v", err)
 }
 
 func guestbookKey(c appengine.Context) *datastore.Key {
@@ -75,6 +87,81 @@ const guestbookTemplateHTML = `
   </body>
 </html>
 `
+
+var rootTemplate = template.Must(template.New("root").Parse(rootTemplateHTML))
+
+const rootTemplateHTML = `
+<html><body>
+{{with .Files}}
+<h1>Your files</h1>
+{{range .}}
+<p>{{index . 0}}({{index . 1}})</p>
+{{end}}
+{{end}}
+<form action="{{.UploadUrl}}" method="POST" enctype="multipart/form-data">
+Upload File: <input type="file" name="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form></body></html>
+`
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+
+	uploadURL, err := blobstore.UploadURL(c, "/upload", &blobstore.UploadURLOptions{StorageBucket: fmt.Sprintf("my_bucket/%v", u.ID)})
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	q := datastore.NewQuery("__GsFileInfo__").KeysOnly().
+		Filter("filename >", fmt.Sprintf("/my_bucket/%v/", u.ID)).
+		Filter("filename <", fmt.Sprintf("/my_bucket/%v0/", u.ID)).
+		Order("filename")
+	it := q.Run(c)
+	files := [][]string{}
+	for {
+		cosa, err := it.Next(nil)
+		if err == datastore.Done {
+			break
+		} else if err != nil {
+			serveError(c, w, err)
+			break
+		}
+
+		info, err := blobstore.Stat(c, appengine.BlobKey(cosa.StringID()))
+		if err != nil {
+			serveError(c, w, err)
+			break
+		}
+		files = append(files, []string{info.Filename, cosa.StringID()})
+	}
+
+	data := struct {
+		UploadUrl *url.URL
+		Files     [][]string
+	}{uploadURL, files}
+	err = rootTemplate.Execute(w, data)
+	if err != nil {
+		c.Errorf("%v", err)
+	}
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	blobs, _, err := blobstore.ParseUpload(r)
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+	file := blobs["file"]
+	if len(file) == 0 {
+		c.Errorf("no file uploaded")
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
 
 func sign(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
